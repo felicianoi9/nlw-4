9f83dbbe3c7a69c53b5ad88bfffd7a7d064daa9d
Rogério Feliciano dos Santos <felicianoi9@gmail.com>
2021-02-23 12:36:09 -0300

    Firsts routes GET and POST working.

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,17 @@ import express from 'express';
 
 const app = express();
 
-app.get("/users", (request, response)=>{
+/**
+ *  first par am  => Route (API recurse)
+ *  secondy param => request, response
+ */
+app.get("/", (request, response)=>{
     // return response.send('Hello World NLW#4');
     return response.json({message:'Hello World - NLW#4'});
+});
+
+app.post("/", (request, response)=>{
+    return response.json({message:"Opa, post deu certo"});
 });
 
 app.listen(3333, ()=> console.log("server is running"));

97f777809c86bcb2b591b2ad07bc650ddad02eb4
Rogério Feliciano dos Santos <felicianoi9@gmail.com>
2021-02-24 20:23:42 -0300

    Test user 1 and 2 ok, but surveys tests fails

--- src/__tests__/Survey.test.ts
+++ src/__tests__/Survey.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import { app } from "../app";
 import createConnection from "../database";
 
-describe("Survey", () => {
+describe("Surveys", () => {
     beforeAll(async () => {
         const connection = await createConnection();
         await connection.runMigrations();
@@ -10,21 +10,21 @@ describe("Survey", () => {
 
     it("Should be able to create a new surveys", async () => {
         const response = await request(app).post("/surveys").send({
-            title: "title example",
+            title: "Title Example",
             description: "Description Example", 
         });
         expect(response.status).toBe(201);
         expect(response.body).toHaveProperty("id");
     }); 
 
-    // it("Should be able to get all surveys", async () => {
-    //     await request(app).post("/surveys").send({
-    //         title: "Title Example2",
-    //         description: "Description Example2", 
-    //     });
-    //     const response = await request(app).get("/surveys");
+    it("Should be able to get all surveys", async () => {
+        await request(app).post("/surveys").send({
+            title: "Title Example2",
+            description: "Description Example2", 
+        });
+        const response = await request(app).get("/surveys");
         
-    //     expect(response.body.length).toBe(2);
-    // }); 
+        expect(response.body.length).toBe(2);
+    }); 
     
 });
